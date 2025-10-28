4c87cab941dcc083a8b4a9cae348f435e654a8d3
Colby Roberts <103100343@student.swin.edu.au>
2025-10-28 22:31:17 +1100

    3D Menu Implementation

---
+++ Assignment/Assets/Scripts/UI/MenuCamControl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MenuCamControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform _currentMount;
+    public float _speed = 0.1f;
+
+    public void setMount(Transform newMount)
+    {
+        _currentMount = newMount;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _currentMount.position, _speed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, _currentMount.rotation, _speed);
+    }
+}
